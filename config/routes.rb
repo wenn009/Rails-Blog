08b5b68b2530659917f1efe51179c6349c7a1775
Xiaowen Huang <wenn009@gmail.com>
2018-07-23 11:19:44 -0400

    able to add user and password

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  resources :articles do
+    resources :comments
+  end
+  
+  resources :users
+
+  root 'users#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
